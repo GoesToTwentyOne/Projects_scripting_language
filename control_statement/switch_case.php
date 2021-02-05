bb1520a89435aa635e4b169364da3105bd7a1947
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2021-02-05 15:50:29 +0600

    add switch case

---
+++ control_statement/switch_case.php
@@ -0,0 +1,38 @@
+<?php
+$ch='E';
+switch($ch){
+  case 'a':
+  echo "This is a and Vowel";
+  break;
+  case 'e':
+  echo "This is e and vowel";
+  break;
+  case 'i':
+  echo "THIS IS i and vowel";
+  break;
+  case 'o':
+  echo "This is o and vowel";
+  break;
+  case 'u':
+  echo "this is u and vowel";
+  break;
+  case 'A':
+  echo "This is A and vowel";
+  break;
+  case 'E':
+  echo "This is E and vowel";
+  break;
+  case 'I':
+  echo "This is I and vowel";
+  break;
+  case 'O':
+  echo "This is o and vowel";
+  break;
+  case 'U':
+  echo "This is U and vowel";
+  break;
+  default:
+  echo "This is consonent letter";
+  break;
+}
+ ?>
