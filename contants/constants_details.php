534088aea30481dc583a0c35b6bd8ab755ade733
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2021-02-05 15:48:53 +0600

    add raw_php_constants

---
+++ contants/constants_details.php
@@ -0,0 +1,10 @@
+<!-- <?php
+define("NIHAD","THIS IS ME",false);//case sensitive
+echo NIHAD;
+ ?> -->
+ <?php
+ define("MESSAGE","Hello JavaTpoint PHP",true);//not case sensitive
+echo MESSAGE, "</br>";
+echo message;
+
+  ?>
